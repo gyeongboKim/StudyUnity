7aed46cfd63cd6a60289037027bab5ac52e6af5f
gyeongboKim <kkbo7253@gmail.com>
2025-07-03 17:02:46 +0900

    feat: Implement initial framework structure

--- Inflearn_MMO/Assets/Script/Managers/Managers.cs
+++ Inflearn_MMO/Assets/Script/Managers/Managers.cs
@@ -8,6 +8,7 @@ public class Managers : MonoBehaviour
     static Managers Instance { get {  Init(); return s_instance; } }  //유일한 매니저를 갖고온다. 프로퍼티 형식
     //start 가 시작 된 경우 Init이 실행되고 혹은 start가 호출되기 이전에 Instance 생성 시 Init이 실행됨.
 
+    DataManager _data = new DataManager();
     InputManager _input = new InputManager();
     PoolManager _pool = new PoolManager();
     ResourceManager _resource = new ResourceManager();
@@ -15,6 +16,7 @@ public class Managers : MonoBehaviour
     SoundManager _sound = new SoundManager();
     UIManager _ui = new UIManager();
 
+    public static DataManager Data { get { return Instance._data; } }
     public static InputManager Input { get { return Instance._input; } }
     public static PoolManager Pool { get { return Instance._pool; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
@@ -48,6 +50,7 @@ public class Managers : MonoBehaviour
             DontDestroyOnLoad(managersRootObject);
             s_instance = managersRootObject.GetComponent<Managers>();
 
+            s_instance._data.Init();    //data의 경우 clear가 불필요
             s_instance._pool.Init();
             s_instance._sound.Init();
         }
--- Inflearn_MMO/Assets/Script/Scenes/GameScene.cs
+++ Inflearn_MMO/Assets/Script/Scenes/GameScene.cs
@@ -4,23 +4,94 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    #region CoroutineTest
+    //class Test
+    //{
+    //    public int id = 0;
+    //}
+    ////연산이 오래걸리거나 복잡한 함수
+    ////예시 길찾기
+    ////1프레임 안에 안되는 경우 일시 정지 후 다음 프레임에 시작
+    ////1. 함수의 상태를 저장/복원 가능
+    ////  엄청 오래걸리는 작업을 잠시 끊거나
+    ////  원하는 타이밍에 함수를 잠시 Stop/복원하는 경우
+    ////2. return -> 원하는 타입으로 가능 (class 도 가능)
+    //class CoroutineTest : IEnumerable
+    //{
+    //    public System.Collections.IEnumerator GetEnumerator()
+    //    {
+    //        for(int i = 0; i < 1000000; i ++)
+    //        {
+    //            if(i % 10000 == 0)
+    //                yield return null;
+    //        }
+    //        //yield return new Test() { id = 1 };
+    //        //yield return null;
+    //        ////yield break;  //아예 빠져 나옴
+    //        //yield return new Test() { id = 2 };
+    //        //yield return new Test() { id = 3 };
+    //        //yield return new Test() { id = 4 };
 
+    //    }
+
+    //    void GenerateItem()
+    //    {
+    //        //예시
+    //        //1. 아이템을 만든다
+    //        //2. DB에 저장한다
+    //        //1이후 2에서 저장이 되는것을 기다리지 않고 다음 로직이 실행되면 로컬에서의 아이템 상태와 DB에서의 아이템이 맞지 않게됨
+    //        //아이템 복사가 일어나거나, 아이템이 이전 상태로 돌아가있거나 하는 문제 발생
+    //    }
+    //    //시간 같은 경우 중앙에서 공용으로 관리할 수 있는 타임 시스템 필요.
+    //    //4초를 세기 위해 매번 체크하고 더하는 방식은 규모가 커지는 경우 큰 리소스 낭비가 됨.
+    //    float deltaTime = 0;
+    //    void ExplodeAfter4Seconds()
+    //    {
+    //        deltaTime += Time.deltaTime;
+    //        if(deltaTime >= 4)
+    //        {
+    //            //로직
+    //        }
+    //    }
+    //}
+    #endregion
+
+    Coroutine co;
 
     protected override void Init()
     {
         base.Init();
+
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inventory>();
 
-        //Test
-        for (int i = 0; i < 5; i++)
-            Managers.Resource.Instantiate("UnityChan");
+        Dictionary<int, Stat> dict = Managers.Data.StatDict;
     }
+
+    //IEnumerator CoStopExplode(float seconds)
+    //{
+    //    Debug.Log("Defusing...");
+    //    yield return new WaitForSeconds(seconds);
+    //    Debug.Log("Defuse Complited!!");
+    //    if(co != null)
+    //    {
+    //        StopCoroutine(co);
+    //        co = null;
+    //    }
+    //}
+
+    //IEnumerator CoExplodeAfterSeconds(float seconds)
+    //{
+    //    Debug.Log("Tic...Tic....");
+    //    yield return new WaitForSeconds(seconds);
+    //    Debug.Log("Boooooom!!!!");
+    //    co = null;
+    //}
 
     public override void Clear()
     {
-        throw new System.NotImplementedException();
+
     }
 
 
